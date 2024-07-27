aaa37bb71dfa6f99a258bcc361127ca09899a0df
Riteshk_007 <codeshorts007@gmail.com>
2024-07-27 16:26:51 +0530

    feat: Add user creation mutation to GraphQL resolver and schema

--- app/Graphql/Resolver.ts
+++ app/Graphql/Resolver.ts
@@ -1,3 +1,55 @@
-const Resolvers = {};
+import bcryptjs from "bcryptjs";
+import { UserInputError } from "apollo-server-micro";
+import prisma from "@/DB/db.config";
+
+const Resolvers = {
+  Mutation: {
+    createUser: async (
+      _: any,
+      args: { email: string; username: string; password: string }
+    ) => {
+      try {
+        const { email, username, password } = args;
+        if (
+          email.trim() === "" ||
+          username.trim() === "" ||
+          password.trim() === ""
+        ) {
+          throw new UserInputError("Please fill in all fields");
+        }
+        if (password.length < 6) {
+          throw new UserInputError("Password must be at least 6 characters");
+        }
+        const existingUser = await prisma.user.findUnique({
+          where: { email: email },
+        });
+        if (existingUser) {
+          throw new UserInputError("User already exists");
+        }
+        const hashedPassword = await bcryptjs.hash(password, 12);
+
+        const usernameCheck = await prisma.user.findUnique({
+          where: { username: username },
+        });
+        if (usernameCheck) {
+          throw new UserInputError("Username already exists");
+        }
+        const user = await prisma.user.create({
+          data: {
+            email,
+            username,
+            password: hashedPassword,
+          },
+        });
+        return user;
+      } catch (error) {
+        if (error instanceof UserInputError) {
+          throw error;
+        }
+        throw new Error("Failed to create user");
+      }
+    },
+  },
+};
 
 export default Resolvers;
--- app/Graphql/Schema.ts
+++ app/Graphql/Schema.ts
@@ -1,5 +1,33 @@
 const Schema = `#graphql
-  
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    password: String!
+    desc: String
+    image: String
+    createdAt: String!
+    updatedAt: String!
+    isPremium: Boolean!
+    links: [Link!]!
+  }
+  type Link {
+  id: ID!
+  url: String!
+  title: String!
+  image: String
+  User: User
+  userId: ID
+}
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(email: String!, username: String!, password: String!): User!
+  }
 `;
 
 export default Schema;
