e3599f5c1a7fd70aed3c6827bcd110372a92145c
Riteshk_007 <codeshorts007@gmail.com>
2024-07-30 12:34:40 +0530

    refactor: Update  username page for username metadata

--- app/(pages)/[username]/page.tsx
+++ app/(pages)/[username]/page.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import UserProfile from "../admin/_components/UserProfile";
+import { Metadata } from "next";
 
-const Username = () => {
+interface PageProps {
+  params: {
+    username: string;
+  };
+}
+
+export const generateMetadata = ({ params }: PageProps): Metadata => {
+  const { username } = params;
+  return {
+    title: `LINK NEST - ${username}`,
+    description: `A collection of all social media links for ${username} in one place.`,
+  };
+};
+
+const UsernamePage = ({ params }: PageProps) => {
+  const { username } = params;
   return (
     <div>
-      <UserProfile gradientFrom="#4a90e2" gradientTo="#81c784" />
+      <UserProfile
+        gradientFrom="#4a90e2"
+        gradientTo="#81c784"
+        // username={username}
+      />
     </div>
   );
 };
 
-export default Username;
+export default UsernamePage;
