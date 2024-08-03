bfa5988b9bdbf62c26f88805ebe8c97a2cfcef18
Riteshk_007 <codeshorts007@gmail.com>
2024-08-03 12:57:07 +0530

    refactor: Update dependencies and add uploadthing library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./src/**/*.{mdx}",
   ],
   prefix: "",
   theme: {
@@ -75,6 +77,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
 
-export default config;
+export default withUt(config);
